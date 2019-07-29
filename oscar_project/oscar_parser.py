fb5958fc9aa254b3b3b4fb92f945fc781534f8da
Ryan Habershaw <ryan_habershaw@brown.edu>
2019-07-29 09:36:51 -0400

    Updated parser with job_id retrieval, working on subprocess module.

--- oscar_project/oscar_parser.py
+++ oscar_project/oscar_parser.py
@@ -1,20 +1,32 @@
 import re
 import subprocess
+import string
 
 
 class OscarParser:
 
-    def __init__(self, job_id):
-        self.job_id = job_id
+    def __init__(self):
+        pass
 
     def get_id(self):
         slurm_log = open('SRS4807080_SRX5884550_1_slurm.out', 'r')
-        rx_id = re.search('\s+ Job ID : \d+', slurm_log)
+        slurm_data = slurm_log.read()
 
-        print rx_id
+        pattern = re.compile('\s+ Job ID : \d+')
+        rx_id = re.findall(pattern, slurm_data)
 
-        return rx_id
+        temp = str(rx_id)
+        all_chars = string.maketrans('', '')
+        just_digits = all_chars.translate(all_chars, string.digits)
+        self.job_id = temp.translate(all_chars, just_digits)
+
+        print self.job_id
+
+        return self.job_id
+
+    def parser(self):
+        subprocess.call('sacct', '-l', '-j', self.job_id, shell=True)
 
 
-    def parser(self, job_id):
-        pass
+parsed_data = OscarParser()
+parsed_data.get_id()
