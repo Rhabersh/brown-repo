7640b03d14253b4e0de20220b920b97a84fd1e35
RHabershaw <51672605+Rhabersh@users.noreply.github.com>
2019-08-15 10:26:41 -0400

    Uploaded SQLite work

---
+++ oscar_project/oscar_table_create.py
@@ -0,0 +1,55 @@
+import sqlite3
+from sqlite3 import Error
+
+#Create class, pass through
+def sqlite_connect():
+    """
+        This function will use the sqlite3 module to connect to a database. If the
+        specified database is not found in the local directory, a new one with
+        the given name will be created.
+
+        :return sqlite_con: The active connection
+    """
+    try:
+        sqlite_con = sqlite3.connect('slurm_database.db')
+        print 'Connection valid - Database active'
+        return sqlite_con
+
+    except Error:
+        print Error
+
+
+def create_table(sqlite_con):
+    """
+        This function creates a table in the sqlite database and establishes the
+        primary key as well as the other columns.
+
+        :param sqlite_con: The active connection to the database is needed so this
+                           function knows where to create/update the table.
+        :return None:
+    """
+    py_cursor = sqlite_con.cursor()
+    py_cursor.execute('CREATE TABLE IF NOT EXISTS resources(jobID integer PRIMARY KEY, memory integer, cores integer, '
+                      'time integer)')
+    sqlite_con.commit()
+
+
+def insert_data(sqlite_con, ph):
+    """
+        This function will insert the parsed data from pysqlite_parser into the table.
+
+        :param sqlite_con: The active connection to the database.
+        :param ph: Placeholder variable for when pysqlite_parser is actually finished.
+                   Will be replaced by other variables for necessary fields.
+        :return None:
+    """
+    py_cursor = sqlite_con.cursor()
+    py_cursor.execute('INSERT OR IGNORE INTO resources(jobID, memory, cores, time) VALUES(?, ?, ?, ?)', ph)
+    sqlite_con.commit()
+
+
+con = sqlite_connect()
+create_table(con)
+
+ph = (1, 2, 3, 4,)
+insert_data(con, ph)
